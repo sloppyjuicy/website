fa9312bb9dfffe9854b2a6685bced7f2da9c1a96
heineiuo <heineiuo@gmail.com>
2020-07-01 22:58:50 +0800

    Initilize

---
+++ src/pages/index.js
@@ -0,0 +1,98 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
+
+const features = [
+  {
+    title: <>Pure TypeScript</>,
+    imageUrl: "img/undraw_docusaurus_mountain.svg",
+    description: (
+      <>
+        RippleDB is totally written in TypeScript, and runs on different
+        platforms after being compiled to JavaScript.
+      </>
+    ),
+  },
+  {
+    title: <>Lightweight</>,
+    imageUrl: "img/undraw_docusaurus_tree.svg",
+    description: (
+      <>
+        RippleDB has only 7k+ source code, and smaller than 1MB after compiled.
+      </>
+    ),
+  },
+  {
+    title: <>Embeddable</>,
+    imageUrl: "img/undraw_docusaurus_react.svg",
+    description: (
+      <>
+        RippleDB can be embedded in node.js application (or other JavaScript
+        Runtime Environments) very easily.
+      </>
+    ),
+  },
+];
+
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx("col col--4", styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                "button button--outline button--secondary button--lg",
+                styles.getStarted
+              )}
+              to={useBaseUrl("docs/")}
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
